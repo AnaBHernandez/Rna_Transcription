d5e07630dee3a73debb4d5c5ea21032fe9cac498
AnaBHernandez <[anahernandezfit@gmail.com]>
2025-03-24 17:11:41 +0100

    Añadido test para transcribir C a G - Test fallido como parte del TDD

--- src/test/java/dev/anabelen/Rna_Transcription/RnaTranscriptionTest.java
+++ src/test/java/dev/anabelen/Rna_Transcription/RnaTranscriptionTest.java
@@ -8,4 +8,10 @@ public class RnaTranscriptionTest {
     public void testTranscribeDnaToRnaSingleNucleotide() {
         assertEquals("C", RnaTranscription.transcribeDnaToRna("G"));
     }
+    @Test
+public void testTranscribeDnaToRnaCytosine() {
+    // Este test verifica que C se transcriba a G
+    assertEquals("G", RnaTranscription.transcribeDnaToRna("C"));
+}
+
 }
